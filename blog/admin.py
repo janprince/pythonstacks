c4bd648ca95c06afa94dc492671dce190ccf87fa
Prince Jan <janprince002@gmail.com>
2020-10-05 06:31:57 +0100

    Registered models on admin. With customisations

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+# Post
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'slug', 'pub_date', 'featured']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('featured',)
+    search_fields = ['title', 'content']
+    actions = ['feature_posts']
+
+    def feature_posts(self, request, queryset):
+        queryset.update(featured=True)
+
+
+# Category
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'tag']
+
+
+# Comment
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'content', 'post', 'created_on', 'active']
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'content')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
+
+
+# Registering models
+admin.site.register(Author)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
