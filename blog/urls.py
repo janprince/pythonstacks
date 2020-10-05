3b8a6539e15aac295e08f50b3f3690f28be0ae6d
Prince Jan <janprince002@gmail.com>
2020-10-05 11:22:29 +0100

    Configured Sidebar Functionality

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path("", views.index, name='index'),
     path("<slug:slug>/", views.detail, name="detail"),
+    path("category/<str:category_tag>", views.category, name='category')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 from django.core.paginator import PageNotAnInteger, Paginator, EmptyPage
+from .forms import CommentForm
 
 
 # global variables
@@ -37,13 +38,51 @@ def index(request):
 
 # Detail view
 def detail(request, slug):
-    post = Post.objects.get(slug=slug)
+    post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active=True) # all active comments associated with this post
+    new_comment = None
 
+    # Comment posted
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)  # pass in the data received to the CommentForm
+        if comment_form.is_valid():
+            # Create a comment but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+
+    else:
+        comment_form = CommentForm()
 
 
     context = {
         'post': post,
         'categories': categories,
+        'comment_form': comment_form,
+        'new_comment': new_comment,
         'recent_posts': recent_posts,
+        'comments': comments,
+
     }
     return render(request, 'blog/detail.html', context)
+
+
+# Category view
+def category(request, category_tag):
+    cat = Category.objects.get(tag=category_tag)
+    posts = cat.posts.all()
+    context = {
+        'cat_name': category_tag,
+        'posts': posts,
+        'categories': categories,
+        'recent_posts': recent_posts,
+    }
+    return render(request, 'blog/category.html', context)
+
+
+def search(request):
+    query = render.GET.get('q')
+    pass
+
