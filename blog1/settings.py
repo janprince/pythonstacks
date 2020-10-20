e9534e071549261f66278a25eca05d5f458ab8f4
Prince Jan Kumi <janprince002@gmail.com>
2020-10-20 20:01:41 +0000

    Updated setting for allowed hosts

--- blog1/settings.py
+++ blog1/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DEBUG"]
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pythonstacks.pythonanywhere.com', 'localhost']
 
 
 # Application definition
