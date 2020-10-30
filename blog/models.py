b012ec025369173d3409b165b8aa5613ef6e56cd
Prince Jan <janprince002@gmail.com>
2020-10-30 13:14:54 +0100

    Models updated

--- blog/models.py
+++ blog/models.py
@@ -64,6 +64,8 @@ class Contact(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField(blank=False)
     message = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return f"Message: {self.name} - {self.message[:150]}..."
