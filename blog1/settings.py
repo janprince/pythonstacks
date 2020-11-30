ddfa30878bb68c89aeba798f0604f42bcd4b8f6e
Prince Jan <janprince002@gmail.com>
2020-11-30 15:22:14 +0100

    Safe spot

--- blog1/settings.py
+++ blog1/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'package_finder',
     'blog',
     'marketing',
-    'courses',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- courses/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Course
-
-
-class CourseAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'free_course', 'priority', 'provider']
-
-
-admin.site.register(Course, CourseAdmin)
