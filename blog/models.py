d7882b67e51d261571f6e6b4ffa5db9a8d9c29e4
Prince Jan <janprince002@gmail.com>
2020-12-09 06:39:04 +0100

    Updated blod meta description

--- blog/models.py
+++ blog/models.py
@@ -29,6 +29,7 @@ class Post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='post_images', blank=True)
     slug = models.SlugField(unique=True)
+    meta_description = models.CharField(max_length=157, blank=False)
     overview = models.TextField()
     content = RichTextUploadingField()
     pub_date = models.DateTimeField(default=timezone.now)      # default=timezone.now - from django.utils import tim...
