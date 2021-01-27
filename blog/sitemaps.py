473124cdf905f8fd0853404ee6a20f82557a8490
Prince Jan <janprince002@gmail.com>
2021-01-27 09:19:53 +0100

    good after book

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -8,7 +8,7 @@ class StaticViewSitemap(Sitemap):
     changefreq = 'daily'
 
     def items(self):
-        return ["home:index", "books:index","packages:index", "blog:index", "home:about", ]
+        return ["home:index", "packages:index", "blog:index", "home:about"]
 
     def location(self, obj):
         return reverse(obj)
