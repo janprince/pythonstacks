aa7090f2386bb5b38977306029eebcbe41f8aa3c
Prince Jan <janprince002@gmail.com>
2021-03-01 12:28:09 +0100

    packages and book app removed

--- blog/models.py
+++ blog/models.py
@@ -15,9 +15,10 @@ class Author(models.Model):
 # Category model
 class Category(models.Model):
     tag = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField(blank=True)
+    slug = models.SlugField(unique=True)
     title_representation = models.CharField(max_length=250, blank=False)
-    meta_description = models.CharField(max_length=155, blank=False)
+    meta_description = models.TextField(max_length=160, blank=False)
+    image = models.ImageField(upload_to="category_images", blank=True)
 
 
     def __str__(self):
@@ -31,7 +32,7 @@ class Post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='post_images', blank=True)
     slug = models.SlugField(unique=True)
-    meta_description = models.CharField(max_length=162, blank=True)
+    meta_description = models.TextField(max_length=162, blank=True)
     content = RichTextUploadingField()
     pub_date = models.DateTimeField(default=timezone.now)      # default=timezone.now - from django.utils import tim...
     categories = models.ManyToManyField(Category, blank=False, related_name='posts')
@@ -61,7 +62,7 @@ class Comment(models.Model):
         ordering = ['-created_on']
 
     def __str__(self):
-        return f"Comment: {self.comment[:20]}... by {self.name}"
+        return f"Comment on ''{self.post}'' by {self.name}"
 
 
 # Comment Reply
