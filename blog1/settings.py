95f5f99a8fe3c35a9c1dfa16ffd7391359881f62
Prince Jan Kumi <janprince002@gmail.com>
2022-09-04 10:30:02 +0000

    include adsense

--- blog1/settings.py
+++ blog1/settings.py
@@ -33,7 +33,6 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ["DEBUG"] == 'True' else False
 
-
 ALLOWED_HOSTS = [] if DEBUG else [ "www.pythonstacks.com", 'pythonstacks.pythonanywhere.com']
 
 
